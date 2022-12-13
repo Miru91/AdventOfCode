b4ed2f6fff15e650f028b434a7188c4db9faa391
Miru91 <marcelquang@gmail.com>
2022-12-13 22:20:25 +0100

    Draft version - wrong result

--- AdventOfCode/supply_stack.py
+++ AdventOfCode/supply_stack.py
@@ -1,13 +1,42 @@
 data = open("supply_stack_input_1.txt", "r")
-order = open("supply_stack_input_2.txt", "r")
-
+orders = open("supply_stack_input_2.txt", "r")
+result = []
 count = 0
+i = 1
+j = 1
 
 for row in data:
     count = count + 1
-    globals()[f"stack_{count}"] = row
+    globals()[f"stack_{count}"] = row.replace("\n", "").split(";")
 
-print(stack_1)
+for order in orders:
+    print(order)
+    order = order.replace("\n", "").split(" ")
+    move = int(order[1])
+    von = globals()[f"stack_{int(order[3])}"]
+    nach = globals()[f"stack_{int(order[5])}"]
 
+    while i <= move:
+        if len(von) > 0:
+            print("von" + str(von))
+            print("nach" + str(nach))
+            nach.append(von[len(von)-1])
+            von.pop()
+            print("von afterwards" + str(von))
+            print("nach afterwards" + str(nach))
+        i += 1
+
+    i = 1
+
+while j <= count:
+    if len(globals()[f"stack_{j}"]) > 0:
+        print(globals()[f"stack_{j}"])
+        result.append(globals()[f"stack_{j}"][-1])
+    j += 1
+
+print(result)
 data.close()
-order.close()
+orders.close()
+
+#VPCDMSLW
+#GHQSWPVM
