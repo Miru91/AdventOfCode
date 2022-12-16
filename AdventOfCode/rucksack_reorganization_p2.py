584373382c4cf46a3656b89a62d360382ee0e383
Miru91 <74835976+Miru91@users.noreply.github.com>
2022-12-16 15:00:25 +0100

    More sexy solution

--- AdventOfCode/rucksack_reorganization_p2.py
+++ AdventOfCode/rucksack_reorganization_p2.py
@@ -4,26 +4,22 @@ data = open("rucksack_input.txt", "r")
 group = []
 result = 0
 
-alpha_dict = dict(zip(string.ascii_letters, range(1, 52)))
-alpha_dict.update({"Z": 52})
+alpha_dict = dict(zip(string.ascii_letters, range(1, 53)))
+print(alpha_dict)
 
 for row in data:
     if len(group) < 3:
         group.append(row[0:-1])
-        print(group)
+        #print(group)
         if len(group) >= 3:
-            match_one = str(set(group[0]).intersection(group[1]))
-            match_two = str(set(group[0]).intersection(group[2]))
-            final_match = str(set(match_one).intersection(match_two))
+            final_match = str(set(group[0]).intersection(group[1],group[2]))
 
             for i in final_match:
                 if i.isalpha():
                     result = result + alpha_dict[i]
-                    print("Der Buchstabe: " + i + " hat den Wert: " + str(alpha_dict[i]))
+                    #print("Der Buchstabe: " + i + " hat den Wert: " + str(alpha_dict[i]))
 
             group = []
-            match_one = ""
-            match_two = ""
             final_match = ""
 
 print(result)
